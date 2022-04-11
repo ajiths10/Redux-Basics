dd480717eafaff0442afd6b43a156adc6e2127dd
Nuclear One <ajithsnair10@gmail.com>
2022-04-11 16:50:11 +0530

    Add 5times and decrement 1times

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter:0 }, action) => {
-    return {
-        counter : state.counter + 1
-    };
+    if(action.type === 'increment') {
+
+        return {
+            counter : state.counter + 1
+        };
+    }else if(action.type === 'decrement'){
+        return {
+            counter : state.counter - 1
+        };
+    }
+    return state
 };
 
 
@@ -18,3 +26,9 @@ const counterSubscriber = () =>{
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'increment'});
+
+store.dispatch({ type: 'decrement'});
